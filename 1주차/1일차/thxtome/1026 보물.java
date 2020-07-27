28354fcdcd025e3056e06622f5cf016dfca7b7b0
thxtome <thxtome531@gmail.com>
2020-07-27 22:26:04 +0900

    주석 추가

--- 1주차/1일차/thxtome/1026 보물.java
+++ 1주차/1일차/thxtome/1026 보물.java
@@ -9,25 +9,33 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int cnt = Integer.parseInt(br.readLine());
+		//순서와 값을 가지는 node클래스 배열을 생성
 		Node[] nodeList = new Node[cnt];
 
+		//입력값을 받아서 스플릿, 스트링 배열로 만듬
 		String[] aArr = br.readLine().split(" ");
 		String[] bArr = br.readLine().split(" ");
 		int[] intAArr = new int[cnt];
 		int answer = 0;
 
+		//두번째 배열은 순서와 값을 넣어서 node로 만듬
+		//두번째 배열을 재배치 하지 않기 위함
 		for (int i = 0; i < cnt; i++) {
 			nodeList[i] = new Node(i, bArr[i]);
 		}
 
+		//첫번째 배열은 숫자로 바꿔서 int[]로 변환
 		for (int i = 0; i < cnt; i++) {
 			intAArr[i] = Integer.parseInt(aArr[i]);
 		}
 
+		//오름차순정렬
 		Arrays.sort(intAArr);
 
+		//내림차순정렬 - node클래스가 comparable를 상속받아 정렬조건을 지정 
 		Arrays.sort(nodeList);
 
+		//a배열에서 값이 낮은순서와 b배열에서 값이 높은 순서로 서로 곱함
 		for (int i = 0; i < cnt; i++) {
 			answer += intAArr[i] * Integer.parseInt(bArr[nodeList[i].seq]);
 		}
@@ -45,6 +53,7 @@ public class Main {
 			this.value = Integer.parseInt(value);
 		}
 
+		//내림차순 정렬
 		@Override
 		public int compareTo(Node o) {
 			return o.value - this.value;
