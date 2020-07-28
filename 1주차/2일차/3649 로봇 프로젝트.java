c71dcc11684e35e1429818e3efce72d700430abc
thxtome <thxtome531@gmail.com>
2020-07-28 21:21:52 +0900

    주석 추가

--- 1주차/2일차/3649 로봇 프로젝트.java
+++ 1주차/2일차/3649 로봇 프로젝트.java
@@ -18,18 +18,23 @@ public class Main {
 			for (int i = 0; i < T; i++) {
 				legos[i] = Integer.parseInt(br.readLine());
 			}
-
+			
+			//개수가 많으면 parallelSort가 유리
 			if (T > 2500) {
 				Arrays.parallelSort(legos);
 			} else {
 				Arrays.sort(legos);
 			}
 
+		
 			for (int i = legos.length - 1; i >= 0; i--) {
+				//이진탐색으로 키를 찾음
 				int key = binarySearch(width - legos[i], i);
+				//맞는 키가 없거나 자기 자신이면 생략
 				if (key == -1 || key == i) {
 					continue;
 				}
+				//찾으면 출력후 다음 케이스 반복
 				System.out.printf("yes %d %d\n", legos[key], legos[i]);
 				continue big;
 			}
